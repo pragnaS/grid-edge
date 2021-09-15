78c58a320d3cfdd9d4d3b060915722637475c5ec
Swetha Berana <swetha.berana@gmail.com>
2021-09-15 15:43:59 -0500

    new CSV files where the word node has been unpurposed

--- models/format_dpmu.py
+++ models/format_dpmu.py
@@ -5,8 +5,8 @@ import os
 #you must have python 3 or above to run this!
 
 #directory is a command line argument, path to folder of multi voltage csv's
-#directory = sys.argv[1]
-directory = 'C:/Users/Swetha/grid-edge/data/target-data/nonscaled_targets'
+directory = sys.argv[1]
+#directory = 'C:/Users/Swetha/grid-edge/data/target-data/nonscaled_targets'
 
 for phase_data in os.listdir(directory):
     #phase data is multi_voltageA.csv,multi_voltageB.csv etc.
@@ -14,7 +14,7 @@ for phase_data in os.listdir(directory):
     with open(directory+ "/" + phase_data, "r") as f:
         lines = f.readlines()
 
-#edited csv's will be put into a folder called "edited_dpmu" within the directory argument
+#edited csv's will be put into the current directories (multi_voltageA.csv, multi_VoltageB.csv, etc.)
     with open(phase_data, "w+") as f:
         for line in lines[8:]:
                 f.write(line)
