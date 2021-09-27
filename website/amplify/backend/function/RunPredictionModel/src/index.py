ceaa83aed399a9f8dab8c7a2d8418d8e4c6f7439
Lucy Zheng <lucy.zheng@utexas.edu>
2021-09-27 14:10:12 -0500

    Removed S3Trigger, configured Python Lambda

--- website/amplify/backend/function/RunPredictionModel/src/index.py
+++ website/amplify/backend/function/RunPredictionModel/src/index.py
@@ -1,15 +1,22 @@
 import json
+import boto3
+import os
+
+
+def retrieve_model(bucket_name, model_key):
+    try:
+        s3 = boto3.resource('s3')
+        model = s3.Object(bucket_name, model_key)
+        return model
+    except Exception as e:
+        return None
+
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    csv_bucket = os.environ['CSV_BUCKET']
+    model_bucket = os.environ['MODEL_BUCKET']
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Handler returns successfully')
+    }
