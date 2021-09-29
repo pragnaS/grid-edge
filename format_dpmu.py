97cc52b6ef30f6fab5b8606fd1cff2b87ed6273a
swetha-berana <90483090+swetha-berana@users.noreply.github.com>
2021-09-29 15:27:21 -0500

    updated format_dpmu script

--- format_dpmu.py
+++ format_dpmu.py
@@ -1,20 +1,35 @@
 import csv
 import sys
 import os
+import shutil
 
 #you must have python 3 or above to run this!
 
-#directory is a command line argument, path to folder of multi voltage csv's
-directory = sys.argv[1]
-#directory = 'C:/Users/Swetha/grid-edge/data/target-data/nonscaled_targets'
+directory = "C:\\Users\\Swetha\\grid-edge\\data\\summer\\targets"
+#directory = str.replace("\\", "/")
+#create new trimmed target directory
+new_trimmed = os.path.join(directory, 'trimmed_targets')
+print (new_trimmed)
+os.makedirs(new_trimmed)
 
-for phase_data in os.listdir(directory):
-    #phase data is multi_voltageA.csv,multi_voltageB.csv etc.
-    #print(phase_data)
-    with open(directory+ "/" + phase_data, "r") as f:
-        lines = f.readlines()
+dirs = os.listdir(directory)
 
-#edited csv's will be put into the current directories (multi_voltageA.csv, multi_VoltageB.csv, etc.)
-    with open(phase_data, "w+") as f:
-        for line in lines[8:]:
-                f.write(line)
+for i in dirs:
+                print (i)
+                if 'aug' in i or 'july' in i or 'june' in i or 'sept' in i:
+                        new_path = new_trimmed + '\\' + i
+                        print (new_path)
+                        try:
+                            os.makedirs(new_path)
+                        except:
+                            print ("already made path")
+                        month_csvs = directory + '\\' + i
+                        print(month_csvs)
+                        for phase_data in os.listdir(month_csvs):
+                                print (phase_data)
+                                with open(month_csvs + '\\' + phase_data, "r") as f:
+                                        lines = f.readlines()
+                                        new_csv = new_path + '\\' + phase_data
+                                        with open(new_csv, "w+") as f:
+                                                for line in lines[8:]:
+                                                        f.write(line)
